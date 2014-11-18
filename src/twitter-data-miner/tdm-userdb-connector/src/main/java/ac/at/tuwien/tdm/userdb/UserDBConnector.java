b60563ed83530aabb37dd5aa840ee1196e829cac
spookyTU <e0405156@student.tuwien.ac.at>
2014-11-18 18:56:09 +0100

    added simple test to UserDBConnector

--- src/twitter-data-miner/tdm-userdb-connector/src/main/java/ac/at/tuwien/tdm/userdb/UserDBConnector.java
+++ src/twitter-data-miner/tdm-userdb-connector/src/main/java/ac/at/tuwien/tdm/userdb/UserDBConnector.java
@@ -3,7 +3,10 @@ package ac.at.tuwien.tdm.userdb;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.h2.tools.RunScript;
 
@@ -12,6 +15,7 @@ public class UserDBConnector {
   private Connection conn;
   private static final String PATH_TO_DB = "./userDB/users";
   private static final String PATH_TO_TABLE = "userTable.sql";
+  private static final String INSERT_USER = "INSERT INTO twitterUsers VALUES (?, ?, ?, ?, ?, ?)";
 
   public void connect() {
     try {
@@ -30,6 +34,39 @@ public class UserDBConnector {
     }
   }
 
+  public void insertUser(Long id, String screenName, String name, String location, int statusesCount, int followers) {
+    try {
+      PreparedStatement p = this.conn.prepareStatement(INSERT_USER);
+      p.setLong(1, id);
+      p.setString(2, screenName);
+      p.setString(3, name);
+      p.setString(4, location);
+      p.setInt(5, statusesCount);
+      p.setInt(6, followers);
+
+      p.execute();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  public void getUsers() {
+    String query = "select * from twitterUsers";
+    try {
+      Statement stmt = this.conn.createStatement();
+
+      ResultSet rs = stmt.executeQuery(query);
+      while (rs.next()) {
+        System.out.println(rs);
+      }
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+  }
+
   public void disconnect() {
     try {
       conn.close();
@@ -42,6 +79,8 @@ public class UserDBConnector {
     UserDBConnector connector = new UserDBConnector();
     connector.connect();
     connector.createUserTable();
+    connector.insertUser(1L, "Test", "test", "bla", 0, 0);
+    connector.getUsers();
   }
 
 }
