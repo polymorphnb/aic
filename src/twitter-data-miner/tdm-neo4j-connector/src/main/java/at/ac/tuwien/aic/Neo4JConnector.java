0235559b728ac90d9beafd89e7d93c973715edb3
spookyTU <e0405156@student.tuwien.ac.at>
2014-11-18 15:02:11 +0100

    added modules to data-miner

---
+++ src/twitter-data-miner/tdm-neo4j-connector/src/main/java/at/ac/tuwien/aic/Neo4JConnector.java
@@ -0,0 +1,10 @@
+package at.ac.tuwien.aic;
+
+
+public interface Neo4JConnector {
+  
+  public void connect();
+  
+  public void disconnect();
+
+}
---
+++ src/twitter-data-miner/tdm-neo4j-connector/src/main/java/at/ac/tuwien/aic/Neo4JConnectorImpl.java
@@ -0,0 +1,20 @@
+package at.ac.tuwien.aic;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+public class Neo4JConnectorImpl implements Neo4JConnector {
+
+  private GraphDatabaseService graphDb;
+  private static final String STORE_DIR = ".";
+  private static final String NEO4J_PROPERTIES_PATH = "neo4j.properties";
+
+  public void connect() {
+    this.graphDb = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(Neo4JConnectorImpl.STORE_DIR)
+        .loadPropertiesFromFile(Neo4JConnectorImpl.NEO4J_PROPERTIES_PATH).newGraphDatabase();
+  }
+
+  public void disconnect() {
+    this.graphDb.shutdown();
+  }
+}
