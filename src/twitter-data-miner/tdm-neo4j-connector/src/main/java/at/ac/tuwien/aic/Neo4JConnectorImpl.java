da3d909820af0cbba313ffbb5ff15455242c5fdf
spookyTU <e0405156@student.tuwien.ac.at>
2014-11-18 16:56:00 +0100

    added db folder for neo4j connector

--- src/twitter-data-miner/tdm-neo4j-connector/src/main/java/at/ac/tuwien/aic/Neo4JConnectorImpl.java
+++ src/twitter-data-miner/tdm-neo4j-connector/src/main/java/at/ac/tuwien/aic/Neo4JConnectorImpl.java
@@ -6,15 +6,17 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 public class Neo4JConnectorImpl implements Neo4JConnector {
 
   private GraphDatabaseService graphDb;
-  private static final String STORE_DIR = ".";
+  private static final String STORE_DIR = "graphDB";
   private static final String NEO4J_PROPERTIES_PATH = "neo4j.properties";
 
   public void connect() {
     this.graphDb = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(Neo4JConnectorImpl.STORE_DIR)
-        .loadPropertiesFromFile(Neo4JConnectorImpl.NEO4J_PROPERTIES_PATH).newGraphDatabase();
+        .loadPropertiesFromURL(this.getClass().getResource("/" + Neo4JConnectorImpl.NEO4J_PROPERTIES_PATH))
+        .newGraphDatabase();
   }
 
   public void disconnect() {
     this.graphDb.shutdown();
   }
+
 }
