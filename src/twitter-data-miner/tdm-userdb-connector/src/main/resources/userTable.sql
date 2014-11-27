9fb1649d7e37fd399fa1cd048c5f6d7ce68a1cca
Carola Gabriel <carola@Carola-Gabriels-MacBook.local>
2014-11-27 19:56:51 +0100

    Datenbankbefüllung implementiert

--- src/twitter-data-miner/tdm-userdb-connector/src/main/resources/userTable.sql
+++ src/twitter-data-miner/tdm-userdb-connector/src/main/resources/userTable.sql
@@ -1,9 +1,14 @@
 CREATE TABLE IF NOT EXISTS twitterUsers (
-	id BIGINT NOT NULL,
+	id BIGINT NOT NULL AUTO_INCREMENT,
 	screenName VARCHAR(255) NOT NULL,
 	name VARCHAR(255) NOT NULL,
 	location VARCHAR(255),
 	statusesCount INTEGER,
-	followers INTEGER,
+	followersCount INTEGER,
+	language VARCHAR(255),
+	favoritesCount INTEGER,
+	friendsCount INTEGER,
+	followerUserIds VARCHAR,
+	friendsUserIds VARCHAR,
 	PRIMARY KEY(id)
 )
