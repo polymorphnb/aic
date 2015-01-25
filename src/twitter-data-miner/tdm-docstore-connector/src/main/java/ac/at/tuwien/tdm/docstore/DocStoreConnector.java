747e02e2ea28c239e168454145343ecf5d78aadb
spookyTU <ralph.hoch@tuwien.ac.at>
2015-01-25 20:04:22 +0100

    format

--- src/twitter-data-miner/tdm-docstore-connector/src/main/java/ac/at/tuwien/tdm/docstore/DocStoreConnector.java
+++ src/twitter-data-miner/tdm-docstore-connector/src/main/java/ac/at/tuwien/tdm/docstore/DocStoreConnector.java
@@ -1,20 +1,32 @@
 package ac.at.tuwien.tdm.docstore;
 
-import java.util.List;
-
 import ac.at.tuwien.tdm.commons.pojo.Ad;
 
+import java.util.List;
+
 public interface DocStoreConnector {
-	public void connect();
-//	public void addTopicToUser(Long user, Long topic);
-	public void createTopicCollection();
-//	public void createUserTweetCollection();
-	public void createAdsCollection();
-	public void dropDatabase();
-	public List<Ad> retrieveAds();
-	//public void getInterestsForUsers(int interestThreshold, Neo4JConnector neo4jdb);
-	public List<Long> retrieveTopics();
-	public Long getTopicIDForKeyword(String keyword);
-//	public double calc_tf_idf_UserTopic(Long userID, Long Topic);
-	public String getTopicForID(Long id);
+
+  public void connect();
+
+  //	public void addTopicToUser(Long user, Long topic);
+  
+  public void createTopicCollection();
+
+  //	public void createUserTweetCollection();
+  
+  public void createAdsCollection();
+
+  public void dropDatabase();
+
+  public List<Ad> retrieveAds();
+
+  //public void getInterestsForUsers(int interestThreshold, Neo4JConnector neo4jdb);
+  
+  public List<Long> retrieveTopics();
+
+  public Long getTopicIDForKeyword(String keyword);
+
+  //	public double calc_tf_idf_UserTopic(Long userID, Long Topic);
+  
+  public String getTopicForID(Long id);
 }
