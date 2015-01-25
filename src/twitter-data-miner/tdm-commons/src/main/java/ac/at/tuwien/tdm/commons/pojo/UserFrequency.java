3785173e8d608b4744803f57fab34bf23471a4d1
spookyTU <ralph.hoch@tuwien.ac.at>
2015-01-25 20:28:00 +0100

    added topic to userFrequency

--- src/twitter-data-miner/tdm-commons/src/main/java/ac/at/tuwien/tdm/commons/pojo/UserFrequency.java
+++ src/twitter-data-miner/tdm-commons/src/main/java/ac/at/tuwien/tdm/commons/pojo/UserFrequency.java
@@ -1,26 +1,50 @@
 package ac.at.tuwien.tdm.commons.pojo;
 
 public class UserFrequency {
-	private String username;
-	private Long id;
-	private double frequence;
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public double getFrequence() {
-		return frequence;
-	}
-	public void setFrequence(double frequence) {
-		this.frequence = frequence;
-	}
-	
+
+  private String username;
+  private Long id;
+  private Long topicID;
+  private String topic;
+  private double frequence;
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+  
+  public Long getTopicID() {
+    return topicID;
+  }
+
+  public void setTopicID(Long topicID) {
+    this.topicID = topicID;
+  }
+
+  public double getFrequence() {
+    return frequence;
+  }
+
+  public void setFrequence(double frequence) {
+    this.frequence = frequence;
+  }
+
+  public String getTopic() {
+    return topic;
+  }
+
+  public void setTopic(String topic) {
+    this.topic = topic;
+  }
 }
